32a27f54ddcbf2f48de83c7197d1888fab5cbc91
Truman <truman.t.kim@gmail.com>
2018-03-27 15:20:43 +0900

    Supplementation as version 0.1.2

--- crypto_util/crypto_util.c
+++ crypto_util/crypto_util.c
@@ -3,7 +3,7 @@
  *
  *      Author  : Truman
  *      Contact : truman.t.kim@gmail.com
- *      Version : 0.1.1
+ *      Version : 0.1.2
  */
 
 #include "crypto_util.h"
@@ -71,4 +71,78 @@ void print_hex(const char* tag, uint8_t *data, size_t data_len) {
     buf[buf_len - 1] = '\0';
     LOG("%s : %s\n", tag, buf);
     free(buf);
+}
+
+void hex_to_bytes(char *src, size_t src_len, uint8_t *dst, size_t dst_len) {
+    uint8_t *p_dst;
+    int i;
+
+    if (src == NULL || dst == NULL
+            || IS_ODD(src_len)
+            || dst_len < (src_len / SINGLE_HEXLEN)) {
+        return;
+    }
+
+    p_dst = dst;
+
+    for (i = 0 ; i < (src_len / SINGLE_HEXLEN) ; i++, p_dst++) {
+        sscanf(src + (i * 2), "%2hhx", p_dst);
+    }
+    return;
+}
+
+void bytes_to_hex(uint8_t *src, size_t src_len, char *dst, size_t dst_len) {
+    const char *hex = "0123456789abcdef";
+    char *p_dst;
+    int i;
+
+    if (src == NULL || dst == NULL
+            || src_len == 0
+            || dst_len < (src_len * SINGLE_HEXLEN)) {
+        return;
+    }
+
+    p_dst = dst;
+
+    for (i = 0 ; i < src_len ; i++) {
+        *(p_dst++) = hex[(src[i] >> 4) & 0x0F];
+        *(p_dst++) = hex[(src[i] & 0x0F)];
+    }
+    return;
+}
+
+void reverse_hex(char *hex, size_t hex_len) {
+    int head, navel, tail;
+    char buff[2];
+
+    if (IS_ODD(hex_len)
+            || hex_len < (SINGLE_HEXLEN * 2)) {
+        return;
+    }
+    for (head = 0, navel = hex_len/SINGLE_HEXLEN, tail =  hex_len - SINGLE_HEXLEN ;
+            head < navel ; head+=SINGLE_HEXLEN, tail-=SINGLE_HEXLEN) {
+        buff[0]     = hex[head+0];
+        buff[1]     = hex[head+1];
+        hex[head+0] = hex[tail+0];
+        hex[head+1] = hex[tail+1];
+        hex[tail+0] = buff[0];
+        hex[tail+1] = buff[1];
+    }
+    return;
+}
+
+void reverse_bytes(uint8_t *bytes, size_t bytes_len) {
+    int head, navel, tail;
+    uint8_t buff;
+
+    if (bytes_len < 2) {
+        return;
+    }
+    for (head = 0, navel = bytes_len/2, tail =  bytes_len - 1 ;
+            head < navel ; head++, tail--) {
+        buff        = bytes[head];
+        bytes[head] = bytes[tail];
+        bytes[tail] = buff;
+    }
+    return;
 }
--- crypto_util/crypto_util.h
+++ crypto_util/crypto_util.h
@@ -3,7 +3,7 @@
  *
  *      Author  : Truman
  *      Contact : truman.t.kim@gmail.com
- *      Version : 0.1.1
+ *      Version : 0.1.2
  */
 
 #ifndef CRYPTO_UTIL_H_
@@ -17,15 +17,32 @@
 #include <stdint.h>
 #include <stdio.h>
 
+#define TRUE            1
+#define FALSE           0
 #define OPENSSL_SUCCESS 1
 #define OPENSSL_FAILURE 0
 
-#define DEFAULT_KEY_LEN 32
+#define ROUND_UPX(i, x) (((i)+((x)-1))&~((x)-1))
+#define ROUND_DWX(i, x) ((i)&~((x)-1))
+#define ROUND_UP8(x)    (((x)+7)&~7)
+#define ROUND_DW8(x)    ((x)&~7)
+#define IN_BYTES(x)     ((x)>>3)
+#define IN_BITS(x)      ((x)<<3)
+#define IS_ODD(x)       (((x)&0x1)==0x1)
+
+#define SET_XYZW(x,y,z,w) ((x) | ((y) << 0x08) | ((z) << 0x10) | ((w) << 0x18))
+#define GET_X(xyzw)       (((xyzw) >> 0x00) & 0xFF)
+#define GET_Y(xyzw)       (((xyzw) >> 0x08) & 0xFF)
+#define GET_Z(xyzw)       (((xyzw) >> 0x10) & 0xFF)
+#define GET_W(xyzw)       (((xyzw) >> 0x18) & 0xFF)
 
 #define IS_FAILED(x)  if (OPENSSL_SUCCESS != (x))
 #define IS_NULL(x) if (NULL == (x))
+
 #define LOG(...) printf(__VA_ARGS__);
 
+#define SINGLE_HEXLEN   2
+#define DEFAULT_KEY_LEN 32
 /*
  * Byte arrays of constant values in ascending order.
  * The arrays are provided as forms of 12, 16, 24 and 32 bytes.
@@ -60,5 +77,8 @@ const uint8_t *get_asc_32();
 int generate_rand_bytes(uint8_t *buf, size_t buf_len);
 void *secure_memset(void *v, int c, size_t n);
 void print_hex(const char* tag, uint8_t *data, size_t data_len);
-
+void hex_to_bytes(char *src, size_t src_len, uint8_t *dst, size_t dst_len);
+void bytes_to_hex(uint8_t *src, size_t src_len, char *dst, size_t dst_len);
+void reverse_hex(char *hex, size_t hex_len);
+void reverse_bytes(uint8_t *bytes, size_t bytes_len);
 #endif /* CRYPTO_UTIL_H_ */
